a063b95accb80d748e94173c04bdfba0e1ed40f1
lualcontre01 <druidlual@gmail.com>
2022-03-29 09:59:15 -0300

    First commit

---
+++ faculty.py
@@ -0,0 +1,29 @@
+# Import necessary packages
+import code
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+# Site URL
+
+end_link = []
+fixed_url="https://www.arch.columbia.edu/faculty"
+fixed_html_content = requests.get(fixed_url).text
+fixed_soup = BeautifulSoup(fixed_html_content, "lxml")
+for link in fixed_soup.find_all('a', {"class":"faculty__name link--blank"}):
+    end_link.append(link.get('href'))
+
+mails = []
+for end in end_link[:50]:
+    end_url = 'https://www.arch.columbia.edu{}'.format(end)
+    end_html_content = requests.get(end_url).text
+    end_soup = BeautifulSoup(end_html_content, "lxml")
+    for mail in end_soup.find_all('div', {"class": "faculty-sidebar__email"}):
+        if '@' in mail.get_text():
+            mails.append(mail.get_text())
+
+with open('mails.txt', 'w') as f:
+    for mail in set(mails[:50]):
+        f.write(mail)
+        f.write('\n')
+    f.close()
